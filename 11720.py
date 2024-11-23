2caf0fa904b58c1e59768538a33366da9c113d4e
mooonyjw <ljungwon91@gmail.com>
2024-11-23 16:45:32 +0900

    숫자의 합

---
+++ 11720.py
@@ -0,0 +1,12 @@
+N = int(input())
+sum = 0
+list = map(int, input())
+for i in list:
+    sum += i
+print(sum)
+
+# N = input()
+# numbers = list(input())
+# sum = 0
+# for i in numbers:
+#     sum += int(i)
